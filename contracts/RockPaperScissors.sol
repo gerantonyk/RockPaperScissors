076905478f5251e78173d1bd22fed032fe2b78cd
gerantonyk <ger.antonyk@gmail.com>
2022-01-26 11:56:59 -0300

    testing done

---
+++ contracts/RockPaperScissors.sol
@@ -0,0 +1,141 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract RockPaperScissors {
+    
+    enum Move {None, Rock, Paper, Scissors }
+    enum GameStatus {Open,CommitReveal,Finished}
+    enum Result {None, Player1, Player2, Draw} 
+    
+    uint public constant MINBET= 0.01 ether;
+    uint public constant TIMEOUT= 10 minutes;
+    
+    //Storage will be cheaper than using a struct
+    uint        public  timeLimit;
+    uint[2]     public  playersBet;
+    address[2]  public  playersAddress;
+    bytes32[2]  public  playersMoveHash;
+    Move[2]     public  playersMove;
+    Result      public  result;
+    GameStatus  public  gameStatus;
+
+    event GameFinished (string,address);
+
+    //checked
+    function play(Move _move, string calldata salt) payable external isNotExpired {
+        require(gameStatus == GameStatus.Open,"Game in progress");
+        require(msg.value>=MINBET, "Must provide a minimun Bet");
+        require(playersAddress[0] != msg.sender,"You can't play against yourself");
+        // console.log("timestamp", block.timestamp);
+        // console.log("constante", TIMEOUT);
+        uint8 i;
+        if (playersAddress[0] != address(0)) {
+            i=1;
+            require(msg.value>=playersBet[0], "Must provide a bet at least equal to player's 1");
+            gameStatus = GameStatus.CommitReveal;
+        }
+        
+        timeLimit = block.timestamp+TIMEOUT;
+        // console.log("timestamp2", block.timestamp);
+        playersBet[i] = msg.value;
+        playersAddress[i] = msg.sender;
+        playersMoveHash[i] = sha256(abi.encodePacked(salt,_move));
+    }
+    //revisar porque puede suceder que no se pueda hacer commit reveal de ninguno de los dos en cuyo caso deberia ser un draw
+    function commitReveal(Move _move, string calldata salt) external isNotExpired {
+        require(gameStatus == GameStatus.CommitReveal,"Not ready for commit reveal");
+        require(playersAddress[0] == msg.sender || playersAddress[1] == msg.sender,"You have to be one of the players");
+        uint8 i;
+        uint8 j = 1;
+        if (playersAddress[1] == msg.sender){
+            i = 1;
+            j = 0; 
+        }
+        require(playersMove[i] == Move.None, "You've already revealed");
+        require(playersMoveHash[i] == sha256(abi.encodePacked(salt,_move)),"Bad commit reveal");
+        playersMove[i] = _move;
+
+        if (playersMove[j] == Move.None) {
+            timeLimit = block.timestamp+TIMEOUT;
+            return;
+        }
+
+
+        gameStatus = GameStatus.Finished;
+        if (playersMove[0] == playersMove[1]) {
+            emit GameFinished ("Draw",address(0));
+            result= Result.Draw;
+        } else if ((playersMove[0] == Move.Rock     && playersMove[1] == Move.Scissors) ||
+                   (playersMove[0] == Move.Paper    && playersMove[1] == Move.Rock)     ||
+                   (playersMove[0] == Move.Scissors && playersMove[1] == Move.Paper)) {
+            emit GameFinished ("Player1 wins",playersAddress[0] );
+            result= Result.Player1;
+        } else {
+            emit GameFinished ("Player2 wins",playersAddress[1] );
+            result= Result.Player2;
+        }
+    }
+
+    function collectBets() external {
+        require(gameStatus == GameStatus.Finished || (timeLimit>0 && block.timestamp>timeLimit ), "Game not finished");
+        
+        address player1 = playersAddress[0];
+        address player2 = playersAddress[1];
+        uint bet1 = playersBet[0];
+        uint bet2 = playersBet[0];
+
+        if (gameStatus == GameStatus.CommitReveal) {
+            if (playersMove[0]== Move.None && playersMove[1]== Move.None) {
+                emit GameFinished ("Draw",address(0));
+                result = Result.Draw;
+            } else if (playersMove[0]== Move.None) {
+                emit GameFinished ("Player2 wins",player2);
+                result = Result.Player2;
+            } else {
+                emit GameFinished ("Player1 wins",player1);
+                result = Result.Player1;
+            }
+        }
+
+        if (gameStatus == GameStatus.Open && playersMoveHash[1]== 0x0) {
+            emit GameFinished ("Player1 wins",player1);
+            result = Result.Player1;
+        }
+
+        Result auxResult =  result;
+        //to avoid reentrancies
+        _reset();
+        if (auxResult == Result.Player1) {
+            payable(player1).transfer(bet1+bet2);
+            return;
+        }
+
+        if (auxResult == Result.Player2) {
+            payable(player2).transfer(bet1+bet2);
+            return;
+        }
+
+        if (auxResult == Result.Draw) {
+            payable(player1).transfer(bet1);
+            payable(player2).transfer(bet2);
+        }
+        // si no esta inalizado pero ya se revelo
+    }
+
+    function _reset() private {
+        timeLimit = 0;
+        delete playersBet;
+        delete playersAddress;
+        delete playersMoveHash;
+        delete playersMove;
+        result = Result.None;
+        gameStatus = GameStatus.Open;
+    }
+
+    modifier isNotExpired() {
+        require(timeLimit==0 || timeLimit >= block.timestamp, "Game expired");
+        _;
+    } 
+}
---
+++ test/sample-test.js
@@ -0,0 +1,331 @@
+const { expect, assert } = require("chai");
+const { parseEther, soliditySha256 } = require("ethers/lib/utils");
+const { ethers } = require("hardhat");
+
+async function  revertEVM(snapshot) {
+  const result= await network.provider.request({
+    method: 'evm_revert',
+    params: [snapshot],
+  });
+  const newSnapshot = await network.provider.request({
+    method: 'evm_snapshot',
+    params: [],
+  });
+
+  return newSnapshot
+}
+
+async function  increaseTime(time) {
+  const result= await network.provider.request({
+    method: 'evm_increaseTime',
+    params: [time],
+  });
+}
+
+describe("RockPaperScissors", function () {
+  let wallet1, wallet2, wallet3;
+  let RockPaperScissors, rockPaperScissors, snapshot;
+  const None = 0; 
+  const Rock = 1;
+  const Paper = 2;
+  const Scissors= 3;
+  const Player1wins = 1; 
+  const Player2wins = 2; 
+  const Draw = 3; 
+  const salt1 = "perros";
+  const salt2 = "gatos";
+  before(async () => {
+    [wallet1, wallet2, wallet3] = await ethers.getSigners();
+    RockPaperScissors = await ethers.getContractFactory("RockPaperScissors");
+    rockPaperScissors = await RockPaperScissors.deploy();
+    await rockPaperScissors.deployed();
+
+    console.log("RockPaperScissors deployed to:", rockPaperScissors.address);
+
+    snapshot = await network.provider.request({
+      method: 'evm_snapshot',
+      params: [],
+    });
+  });
+
+  describe("Play", function () {
+    beforeEach(async function() {
+      snapshot = await revertEVM(snapshot)
+    })  
+    it("Should revert if the move is incorrect", async function () {
+      await expect(rockPaperScissors.play(13,salt1,{value: parseEther('0.001')})).to.be.reverted
+    });
+    it("Should revert if the bet is lower than the min bet", async function () {
+      await expect(rockPaperScissors.play(Rock,salt1,{value: parseEther('0.001')})).to.be.revertedWith("Must provide a minimun Bet");
+    });
+
+    it("Should not allow same user to play twice", async function () {
+      await rockPaperScissors.play(Rock,salt1,{value: parseEther('0.01')})
+      await expect(rockPaperScissors.play(Scissors,salt1,{value: parseEther('0.01')})).to.be.revertedWith("You can't play against yourself")
+    });
+
+    it("Should revert if the bet is lower than the first player's bet", async function () {
+      await rockPaperScissors.play(Rock,salt1,{value: parseEther('0.02')})
+      await expect(rockPaperScissors.connect(wallet2).play(Rock,salt1,{value: parseEther('0.01')})).to.be.revertedWith("Must provide a bet at least equal to player's 1")
+    });
+
+    it("Should not allow players to play on an expired game", async function () {
+      await rockPaperScissors.play(Rock,salt1,{value: parseEther('0.01')})
+      increaseTime(601)
+      await expect(rockPaperScissors.connect(wallet2).play(Rock,salt1,{value: parseEther('0.01')})).to.be.revertedWith("Game expired")
+    });
+
+    it("Should not allow other players to play on an started game", async function () {
+      await rockPaperScissors.play(Rock,salt1,{value: parseEther('0.01')})
+      await rockPaperScissors.connect(wallet2).play(Rock,salt2,{value: parseEther('0.01')})
+      await expect(rockPaperScissors.connect( wallet3).play(Rock,salt1,{value: parseEther('0.01')})).to.be.revertedWith("Game in progress")
+    });
+
+
+    it("Should accept a valid move and a valid bet", async function () {
+      await rockPaperScissors.play(Rock,salt1,{value: parseEther('0.01')});
+      assert.equal(await rockPaperScissors.playersAddress(0), wallet1.address);
+      const bet = await rockPaperScissors.playersBet(0);
+      assert.equal(bet.toString(), parseEther('0.01').toString());
+      const hash = await rockPaperScissors.playersMoveHash(0);
+      const recreatedHash = soliditySha256(['string','uint8' ],[salt1,Rock]);
+      assert.equal(hash, recreatedHash)
+
+      // assert.equal(await rockPaperScissors.playersAddress(0), wallet1.address)
+    });
+
+    it("Should update the time limit", async function () {
+      await rockPaperScissors.play(Rock,salt1,{value: parseEther('0.01')});
+      const timeLimit = await rockPaperScissors.timeLimit();
+      assert.isAbove(timeLimit.toNumber(), 0)
+    });
+
+    it("Should accept a valid move and a valid bet for a player2", async function () {
+      await rockPaperScissors.play(Rock,salt1,{value: parseEther('0.01')});
+      await rockPaperScissors.connect(wallet2).play(Scissors,salt2,{value: parseEther('0.01')});
+      assert.equal(await rockPaperScissors.playersAddress(1), wallet2.address);
+      const bet = await rockPaperScissors.playersBet(1);
+      assert.equal(bet.toString(), parseEther('0.01').toString());
+      const hash = await rockPaperScissors.playersMoveHash(1);
+      const recreatedHash = soliditySha256(['string','uint8' ],[salt2,Scissors]);
+      assert.equal(hash, recreatedHash)
+
+      // assert.equal(await rockPaperScissors.playersAddress(0), wallet1.address)
+    });
+
+    it("Should update the time limit again", async function () {
+      await rockPaperScissors.play(Rock,salt1,{value: parseEther('0.01')});
+      const timeLimit = await rockPaperScissors.timeLimit();
+      await rockPaperScissors.connect(wallet2).play(Scissors,salt2,{value: parseEther('0.01')});
+      const timeLimit2 = await rockPaperScissors.timeLimit();
+      assert.isAbove(timeLimit2.toNumber(),timeLimit.toNumber())
+    });
+
+    it("Should change the game status to CommitReveal", async function () {
+      await rockPaperScissors.play(Rock,salt1,{value: parseEther('0.01')});
+      await rockPaperScissors.connect(wallet2).play(Scissors,salt2,{value: parseEther('0.01')});
+      const gameStatus = await rockPaperScissors.gameStatus();
+      assert.equal(gameStatus,1)
+    });
+
+    it("Should be reverted when the game status is CommitReveal", async function () {
+      await rockPaperScissors.play(Rock,salt1,{value: parseEther('0.01')});
+      await rockPaperScissors.connect(wallet2).play(Scissors,salt2,{value: parseEther('0.01')});
+      await expect(rockPaperScissors.connect( wallet3).play(Rock,salt1,{value: parseEther('0.01')})).to.be.revertedWith("Game in progress")
+    });
+  });
+
+  describe("CommitReveal", function () {
+
+    it("Should not allow players to commit reveal until both player have played", async function () {
+      snapshot = await revertEVM(snapshot)
+      await expect(rockPaperScissors.commitReveal(Rock,salt1)).to.be.revertedWith("Not ready for commit reveal")
+    });
+
+    it("Should declare Rock winner against Scissors", async function () {
+      snapshot = await revertEVM(snapshot)
+      await rockPaperScissors.play(Rock,salt1,{value: parseEther('0.01')});
+      await rockPaperScissors.connect(wallet2).play(Scissors,salt2,{value: parseEther('0.01')});
+      await rockPaperScissors.commitReveal(Rock,salt1);
+      await rockPaperScissors.connect(wallet2).commitReveal(Scissors,salt2);
+      const result = await rockPaperScissors.result();
+      assert.equal(result,Player1wins)
+    });
+
+    it("Should declare Scissors winner against Paper", async function () {
+      snapshot = await revertEVM(snapshot)
+      await rockPaperScissors.play(Scissors,salt1,{value: parseEther('0.01')});
+      await rockPaperScissors.connect(wallet2).play(Paper,salt2,{value: parseEther('0.01')});
+      await rockPaperScissors.commitReveal(Scissors,salt1);
+      await rockPaperScissors.connect(wallet2).commitReveal(Paper,salt2);
+      const result = await rockPaperScissors.result();
+      assert.equal(result,Player1wins)
+    });
+
+    it("Should declare Paper winner against Rock", async function () {
+      snapshot = await revertEVM(snapshot)
+      await rockPaperScissors.play(Rock,salt1,{value: parseEther('0.01')});
+      await rockPaperScissors.connect(wallet2).play(Paper,salt2,{value: parseEther('0.01')});
+      await rockPaperScissors.commitReveal(Rock,salt1);
+      await rockPaperScissors.connect(wallet2).commitReveal(Paper,salt2);
+      const result = await rockPaperScissors.result();
+      assert.equal(result,Player2wins)
+    });
+
+    it("Should declare Draw when both players play the same move", async function () {
+      snapshot = await revertEVM(snapshot)
+      await rockPaperScissors.play(Rock,salt1,{value: parseEther('0.01')});
+      await rockPaperScissors.connect(wallet2).play(Rock,salt2,{value: parseEther('0.01')});
+      await rockPaperScissors.commitReveal(Rock,salt1);
+      await rockPaperScissors.connect(wallet2).commitReveal(Rock,salt2);
+      const result = await rockPaperScissors.result();
+      assert.equal(result,Draw)
+    });
+
+    beforeEach(async function() {
+      snapshot = await revertEVM(snapshot)
+      await rockPaperScissors.play(Rock,salt1,{value: parseEther('0.01')});
+      await rockPaperScissors.connect(wallet2).play(Scissors,salt2,{value: parseEther('0.01')});      
+    }) 
+
+    it("Should not allow a third player to commit reveal", async function () {
+
+      await expect(rockPaperScissors.connect(wallet3).commitReveal(Rock,salt1)).to.be.revertedWith("You have to be one of the players")
+    });
+
+    it("Should not allow players to commit reveal on an expired game", async function () {
+      increaseTime(601)
+      await expect(rockPaperScissors.commitReveal(Rock,salt1)).to.be.revertedWith("Game expired")
+    });
+   
+    it("Should not allow players to reveal more than once", async function () {
+      rockPaperScissors.commitReveal(Rock,salt1)
+      await expect(rockPaperScissors.commitReveal(Rock,salt1)).to.be.revertedWith("You've already revealed")
+    });
+   
+    it("Should not allow players to reveal with a different password", async function () {
+      await expect(rockPaperScissors.commitReveal(Rock,salt2)).to.be.revertedWith("Bad commit reveal")
+    });
+
+    it("Should not allow players to reveal with a different move", async function () {
+      await expect(rockPaperScissors.commitReveal(Rock,salt2)).to.be.revertedWith("Bad commit reveal")
+    });
+
+    it("Should update the time limit again", async function () {
+      const timeLimit = await rockPaperScissors.timeLimit();
+      await rockPaperScissors.connect(wallet2).commitReveal(Scissors,salt2);
+      const timeLimit2 = await rockPaperScissors.timeLimit();
+      assert.isAbove(timeLimit2.toNumber(),timeLimit.toNumber())
+    });
+
+    it("Should update player's move when calls commitReveal successfully", async function () {
+      await rockPaperScissors.commitReveal(Rock,salt1);
+      const move = await rockPaperScissors.playersMove(0);
+      assert.equal(move,1)
+    });
+
+    it("Should update player2's move when calls commitReveal successfully", async function () {
+      await rockPaperScissors.commitReveal(Rock,salt1);
+      await rockPaperScissors.connect(wallet2).commitReveal(Scissors,salt2)
+      const move = await rockPaperScissors.playersMove(1);
+      assert.equal(move,3)
+    });
+
+    it("Should emmit an event when both players reveal", async function () {
+      await rockPaperScissors.commitReveal(Rock,salt1);
+      await expect(rockPaperScissors.connect(wallet2).commitReveal(Scissors,salt2))
+      .to.emit(rockPaperScissors, "GameFinished")
+      .withArgs("Player1 wins",wallet1.address )
+    });
+
+    it("Should change the game status to finished", async function () {
+      await rockPaperScissors.commitReveal(Rock,salt1);
+      await rockPaperScissors.connect(wallet2).commitReveal(Scissors,salt2)
+      const gameStatus = await rockPaperScissors.gameStatus();
+      assert.equal(gameStatus,2)
+    });
+
+  });
+
+  describe("CollectBets", function () {
+    beforeEach(async function() {
+      snapshot = await revertEVM(snapshot)
+    })  
+
+    it("Should revert if the game is not finished nor expired", async function () {
+      await expect(rockPaperScissors.collectBets()).to.be.revertedWith("Game not finished")
+    });
+    
+    it("Should allow to collect bet if the game is finished", async function () {
+      await rockPaperScissors.play(Rock,salt1,{value: parseEther('100')});
+      await rockPaperScissors.connect(wallet2).play(Paper,salt2,{value: parseEther('100')});
+      await rockPaperScissors.commitReveal(Rock,salt1);
+      await rockPaperScissors.connect(wallet2).commitReveal(Paper,salt2);
+      const balanceBefore = await ethers.provider.getBalance(wallet2.address)
+      await rockPaperScissors.collectBets();
+      const balanceAfter = await ethers.provider.getBalance(wallet2.address)
+      assert.isAbove(balanceAfter,balanceBefore)
+    });
+
+    it("Should allow to collect bet if the game is expired", async function () {
+      await rockPaperScissors.play(Rock,salt1,{value: parseEther('100')});
+      await rockPaperScissors.connect(wallet2).play(Scissors,salt2,{value: parseEther('100')});
+      increaseTime(601)
+      const balanceBefore = await ethers.provider.getBalance(wallet1.address)
+      await rockPaperScissors.collectBets();
+      const balanceAfter = await ethers.provider.getBalance(wallet1.address)
+      assert.isAbove(balanceAfter,balanceBefore)
+    });    
+
+    it("Should emit a GameFinished event if the game is expired and the outcome was not declare yet", async function () {
+      await rockPaperScissors.play(Rock,salt1,{value: parseEther('100')});
+      await rockPaperScissors.connect(wallet2).play(Scissors,salt2,{value: parseEther('100')});
+      increaseTime(601)
+      await expect(rockPaperScissors.collectBets())
+      .to.emit(rockPaperScissors, "GameFinished")
+      .withArgs("Draw",'0x'+'0'.repeat(40) )
+    });       
+
+    it("Should returns the bets if the outcome is Draw", async function () {
+      await rockPaperScissors.play(Rock,salt1,{value: parseEther('100')});
+      await rockPaperScissors.connect(wallet2).play(Rock,salt2,{value: parseEther('100')});
+      await rockPaperScissors.commitReveal(Rock,salt1);
+      await rockPaperScissors.connect(wallet2).commitReveal(Rock,salt2);
+      const balanceBefore1 = await ethers.provider.getBalance(wallet1.address)
+      const balanceBefore2 = await ethers.provider.getBalance(wallet2.address)
+      await rockPaperScissors.collectBets();
+      const balanceAfter1 = await ethers.provider.getBalance(wallet1.address)
+      const balanceAfter2 = await ethers.provider.getBalance(wallet2.address)
+      const rockPaperScissorsBalance = await ethers.provider.getBalance(rockPaperScissors.address)
+      assert.isAbove(balanceAfter1,balanceBefore1)
+      assert.isAbove(balanceAfter2,balanceBefore2)
+      assert.equal(rockPaperScissorsBalance,0)
+    });
+
+    it("Should reset the game once the bets are collected", async function () {
+      await rockPaperScissors.play(Rock,salt1,{value: parseEther('100')});
+      await rockPaperScissors.connect(wallet2).play(Rock,salt2,{value: parseEther('100')});
+      await rockPaperScissors.commitReveal(Rock,salt1);
+      await rockPaperScissors.connect(wallet2).commitReveal(Rock,salt2);
+
+      await rockPaperScissors.collectBets();
+
+      const timeLimit = await rockPaperScissors.timeLimit();
+      const playersBet = await rockPaperScissors.playersBet(0);
+      const playersAddress = await rockPaperScissors.playersAddress(0);
+      const playersMoveHash = await rockPaperScissors.playersMoveHash(0);
+      const playersMove = await rockPaperScissors.playersMove(0);
+      const result = await rockPaperScissors.result();
+      const gameStatus = await rockPaperScissors.gameStatus();
+      assert.equal(timeLimit,0);
+      assert.equal(playersBet,0);
+      assert.equal(playersAddress,0);
+      assert.equal(playersMoveHash,0);
+      assert.equal(playersMove,0);
+      assert.equal(result,0);
+      assert.equal(gameStatus,0);
+    });
+  });
+});
+
+
